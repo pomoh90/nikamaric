67fc6e9133a4e8260f0cf31bfd9777989e13e25b
pomoh90 <dacaball14121990@gmail.com>
2025-03-17 20:36:25 +0100

    phonennum

--- app/user_info.js
+++ app/user_info.js
@@ -1,9 +1,9 @@
 const userInfo = {
   contact: [
-    { text: 'Phone', url: 'tel:+1222222222', label: '+1 (222) 222-222' },
+    { text: 'Phone', url: 'tel:+13323331350', label: '+1 (332) 333-1350' },
     { text: 'Email', url: 'mailto:nikamaric960@gmail.com', label: 'nikamaric960@gmail.com' },
     { text: 'Erotic Reviews', url: '', label: ''},
-    { text: 'Twitter', url: '', label: ''},
+    // { text: 'Twitter', url: '', label: ''},
   ],
   donations: [
     { text: 'Incall 1hr - $700' },
